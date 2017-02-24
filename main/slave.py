6a7791d0aea5f038a2586e286b40219df50bb92f
zhanghongtao <zhanghongtao@mioji.com>
2017-02-24 17:55:07 +0800

    test over

--- main/slave.py
+++ main/slave.py
@@ -7,15 +7,9 @@ import time
 from slaveinfo import SlaveInfo
 
 class Slave:
-    def __init__(self, self_host, port, master_host, workers):
-        #self.__server = http_server.HttpServer("0.0.0.0", port)
-        #self.__client
-        #self.__master_host
+    def __init__(self, host, workers):
         #self.info = SlaveInfo()
-        #self.info.server
-        #self.info.server_ip
-        #self.info.path
-        
+        self.__host = host
         self.__timer = timer.Timer(10, self.heartbeat)
         self.__sem = threading.Semaphore()
         self.__workers = workers
@@ -23,7 +17,6 @@ class Slave:
     def run(self):
         self.__timer.start()
         self.__workers.start()
-        #self.__server.run()
 
     def register(self):
         pass
@@ -48,7 +41,7 @@ if __name__ == "__main__":
     from workers import Workers
     workers = Workers(workload, work, 3, 40)
 
-    slave = Slave("127.0.0.1", 8888, "10.10.99.53", workers)
+    slave = Slave("10.10.99.53", workers)
     slave.run()
 
 
